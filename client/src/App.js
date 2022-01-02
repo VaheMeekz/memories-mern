98d4533a5e5b8dee02f4e9726fc5792651335c02
Vahe <vaheemkrtchyan@gmail.com>
2022-01-02 13:00:10 +0400

    apis

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import momories from "../src/images/vmLogo.svg"
+import Posts from "./components/posts/Posts";
+import Forms from "./components/forms/Forms";
+import useStyles from "./style"
 
 const App = () => {
+    const classes = useStyles()
+
     return (
-        <div>
-         app
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">
+                    Memories
+                </Typography>
+                <img className={classes.image} src={momories} alt="memories" height="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Forms/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 };
 
